45f7da2af3fc29e6508e6db88793f18f1447eb2a
Ryan Dabler <rpdabler@gmail.com>
2022-04-22 09:02:35 -0600

    feat: implement public Caesar cipher package
    Allows for typical encryption and decryption functionality as well as cracking Caesar cipher using brute force attacks or frequency analysis

---
+++ caesar/caesar.go
@@ -0,0 +1,82 @@
+package caesar
+
+import "cryptgo/internal/util"
+
+type Ranker func([]string) []string
+
+func encrypt(text []rune, key rune, alphabet []rune) []rune {
+	runes := []rune(text)
+	mod := len(alphabet)
+	shift := indexOf(alphabet, key)
+	cipher := make([]rune, len(runes))
+
+	for i, r := range runes {
+		pos := indexOf(alphabet, r)
+		newPos := (pos - shift + mod) % mod
+		cipher[i] = alphabet[newPos]
+	}
+
+	return cipher
+}
+
+func decrypt(text []rune, key rune, alphabet []rune) []rune {
+	runes := []rune(text)
+	mod := len(alphabet)
+	shift := indexOf(alphabet, key)
+	cipher := make([]rune, len(runes))
+
+	for i, r := range runes {
+		pos := indexOf(alphabet, r)
+		newPos := (pos + shift) % mod
+		cipher[i] = alphabet[newPos]
+	}
+
+	return cipher
+}
+
+// Encrypt a string `text` of alphabet `alphabet` shifting left
+// by a constant `key`.
+//
+// Each character of `text` will be left-shifted by the
+// numerical value of `key` as it exists in the alphabet.
+// This means the magnitude of left-shifting is based on
+// where `k` appears in the alphabet. Thus if we shift
+// by 'b' in alphabet 'abc' we would shift by 1 whereas
+// in the alphabet 'acb' we would shift by 2.
+func Encrypt(text string, key string, alphabet string) string {
+	k := []rune(key)[0]
+	cipher := encrypt([]rune(text), k, []rune(alphabet))
+	return string(cipher)
+}
+
+// Decrypt a string `text` of alphabet `alphabet` shifting right
+// by a constant `key`.
+//
+// Each character of `text` will be right-shifted by the
+// numerical value of `key` as it exists in the alphabet.
+// This means the magnitude of right-shifting is based on
+// where `k` appears in the alphabet. Thus if we shift
+// by 'b' in alphabet 'abc' we would shift by 1 whereas
+// in the alphabet 'acb' we would shift by 2.
+func Decrypt(text string, key string, alphabet string) string {
+	k := []rune(key)[0]
+	plain := decrypt([]rune(text), k, []rune(alphabet))
+	return string(plain)
+}
+
+func CrackForce(cipher string, alphabet string, ranker Ranker) []string {
+	ps := permutations([]rune(cipher), []rune(alphabet))
+	return ranker(toStrings(ps))
+}
+
+func CrackFreq(cipher string, alphabet string, frequencies map[string]float64, ranker Ranker) []string {
+	mostFreq := sortedFreqs(frequencies)
+	top5 := util.Take(mostFreq, 5)
+	possibles := make([]string, len(top5))
+
+	for i, v := range top5 {
+		possibles[i] = Decrypt(cipher, v, alphabet)
+	}
+
+	return ranker(possibles)
+}
---
+++ caesar/utilities.go
@@ -0,0 +1,63 @@
+package caesar
+
+import (
+	"cryptgo/internal/util"
+	"sort"
+)
+
+func permutations(text []rune, alphabet []rune) [][]rune {
+	ps := make([][]rune, len(alphabet))
+
+	for i, v := range alphabet {
+		ps[i] = decrypt(text, v, alphabet)
+	}
+
+	return ps
+}
+
+func toStrings(rs [][]rune) []string {
+	ss := make([]string, len(rs))
+
+	for i, v := range rs {
+		ss[i] = string(v)
+	}
+
+	return ss
+}
+
+func indexOf(arr []rune, v rune) int {
+	for i, e := range arr {
+		if e == v {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func sortedFreqs(freqs map[string]float64) []string {
+	intermediate := make([]struct {
+		k string
+		v float64
+	}, len(freqs))
+	i := 0
+
+	for k, v := range freqs {
+		intermediate[i] = struct {
+			k string
+			v float64
+		}{k, v}
+		i++
+	}
+
+	sort.Slice(intermediate, func(i, j int) bool {
+		return intermediate[j].v < intermediate[i].v
+	})
+
+	return util.MapSlice(intermediate, func(e struct {
+		k string
+		v float64
+	}) string {
+		return e.k
+	})
+}
